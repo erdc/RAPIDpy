ad8621c062e40070118d1cc337f7809f05487874
snowman2 <alansnow21@gmail.com>
2015-09-04 14:36:33 -0500

    bug fixes and new version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='RAPIDpy',
-    version='1.0.2',
+    version='1.0.3',
     description='Python scripting interface for the RAPID progam. More information about installation and the input parameters can be found at http://rapid-hub.org. The source code for RAPID is located at https://github.com/c-h-david/rapid/.',
     keywords='RAPID',
     author='Alan Dee Snow',
     author_email='alan.d.snow@usace.army.mil',
     url='https://github.com/erdc-cm/RAPIDpy',
-    download_url='https://github.com/erdc-cm/RAPIDpy/tarballs/1.0.2',
+    download_url='https://github.com/erdc-cm/RAPIDpy/tarballs/1.0.3',
     license='MIT',
     packages=['RAPIDpy'],
-    install_requires=['netCDF4', 'numpy'],
+    install_requires=['netCDF4', 'numpy', 'requests'],
 )

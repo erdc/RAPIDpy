cb3eeac5ab61a52c6d78e1a334b323b59406952b
snowman2 <alansnow21@gmail.com>
2016-01-20 15:21:11 -0600

    added CF compliant tests

--- tests/test_rapidpy.py
+++ tests/test_rapidpy.py
@@ -7,15 +7,16 @@
 ##  Copyright © 2015 Alan D Snow. All rights reserved.
 ##
 
+from datetime import datetime
 from filecmp import cmp as fcmp
-from nose.tools import raises, ok_
+from netCDF4 import Dataset
+from nose.tools import ok_
 import os
 from shutil import copy
 
 
 #local import
-import sys
-from RAPIDpy.rapid import RAPID
+from RAPIDpy.rapid import RAPID, compare_qout_files
 
 
 #GLOBAL VARIABLES
@@ -169,7 +170,16 @@ def test_update_rapid_numbers_input_file():
     except OSError:
         pass
 
-def test_update_rapid_numbers_input_file():
+def test_qout_same():
+    """
+    Test function to compare RAPID simulation Qout
+    """
+    input_qout_file = os.path.join(COMPARE_DATA_PATH, 'Qout_erai_t511_3hr_19800101.nc')
+    input_qout_file_cf = os.path.join(COMPARE_DATA_PATH, 'Qout_erai_t511_3hr_19800101_CF.nc')
+   
+    ok_(compare_qout_files(input_qout_file, input_qout_file_cf))
+
+def test_run_rapid_simulation():
     """
     Test Running RAPID Simulation
     """
@@ -193,20 +203,68 @@ def test_update_rapid_numbers_input_file():
                           Vlat_file=os.path.join(rapid_input_data_path, 'm3_riv_bas_erai_t511_3hr_19800101.nc'),
                           k_file=os.path.join(rapid_input_data_path, 'k.csv'),
                           x_file=os.path.join(rapid_input_data_path, 'x.csv'),
+                          ZS_dtM=10800,
+                          ZS_dtR=900,
+                          ZS_TauM=86400,
+                          ZS_TauR=10800,
                           Qout_file=generated_qout_file
                          )
-
+    rapid_manager.update_reach_number_data()
     rapid_manager.run()
 
     generated_qout_file_solution = os.path.join(COMPARE_DATA_PATH,
                                                 'Qout_erai_t511_3hr_19800101.nc')
 
     ok_(fcmp(generated_qout_file, generated_qout_file_solution))
-    
     try:
         os.remove(generated_qout_file)
     except OSError:
         pass
+    
+def test_convert_file_to_be_cf_compliant():
+    """
+    Test Convert RAPID Output to be CF Compliant
+    """
+    
+    print "TEST 5: TEST CONVERT RAPID OUTPUT TO CF COMPLIANT (COMID_LAT_LON_Z)"
+
+    rapid_input_data_path = os.path.join(INPUT_DATA_PATH,
+                                         'nfie_texas_gulf_region-huc_2_12')
+
+    input_qout_file = os.path.join(COMPARE_DATA_PATH, 'Qout_erai_t511_3hr_19800101.nc')
+    temp_qout_file = os.path.join(OUTPUT_DATA_PATH, 'Qout_erai_t511_3hr_19800101_test_cf.nc')
+    copy(input_qout_file, temp_qout_file)
+    
+    rapid_manager = RAPID(rapid_executable_location="",
+                          Qout_file=temp_qout_file,
+                          rapid_connect_file=os.path.join(rapid_input_data_path, 'rapid_connect.csv'),
+                          ZS_TauR=3*3600)
+
+    rapid_manager.make_output_CF_compliant(simulation_start_datetime=datetime(1980, 1, 1),
+                                           comid_lat_lon_z_file=os.path.join(rapid_input_data_path, 'comid_lat_lon_z.csv'),
+                                           project_name="ERA Interim (T511 Grid) 3 Hourly Runoff Based Historical flows by US Army ERDC")
+
+    cf_qout_file_solution = os.path.join(COMPARE_DATA_PATH,
+                                         'Qout_erai_t511_3hr_19800101_CF.nc')
+
+    #check Qout    
+    ok_(compare_qout_files(temp_qout_file, cf_qout_file_solution))
+
+    #check other info in netcdf file
+    d1 = Dataset(temp_qout_file)
+    d2 = Dataset(cf_qout_file_solution)
+    ok_(d1.dimensions.keys() == d2.dimensions.keys())
+    ok_(d1.variables.keys() == d2.variables.keys())
+    ok_((d1.variables['time'][:] == d1.variables['time'][:]).all())
+    ok_((d1.variables['rivid'][:] == d1.variables['rivid'][:]).all())
+    d1.close()
+    d2.close()
+    
+    try:
+        os.remove(temp_qout_file)
+    except OSError:
+        pass
+
 
 if __name__ == '__main__':
     import nose

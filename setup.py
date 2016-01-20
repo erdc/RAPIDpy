70bd9504013febbe83503be2f6a442a3ed3cc25b
snowman2 <alansnow21@gmail.com>
2016-01-20 08:16:05 -0600

    added pytz to prereqs

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='RAPIDpy',
-    version='1.1.3',
+    version='2.0.0',
     description='Python scripting interface for the RAPID progam. More information about installation and the input parameters can be found at http://rapid-hub.org. The source code for RAPID is located at https://github.com/c-h-david/rapid/.',
     keywords='RAPID',
     author='Alan Dee Snow',
     author_email='alan.d.snow@usace.army.mil',
     url='https://github.com/erdc-cm/RAPIDpy',
-    download_url='https://github.com/erdc-cm/RAPIDpy/tarballs/1.1.3',
+    download_url='https://github.com/erdc-cm/RAPIDpy/tarballs/2.0.0',
     license='MIT',
     packages=['RAPIDpy'],
-    install_requires=['netCDF4', 'numpy', 'requests', 'dateutil'],
+    install_requires=['dateutil', 'netCDF4', 'numpy', 'pytz', 'requests'],
 )

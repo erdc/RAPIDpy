541e437bdeb7bd2ed7c3e3b1ee2a144b982bfc9c
snowman2 <alan.d.snow@usace.army.mil>
2016-02-04 11:53:47 -0600

    added goodness of fit tests and added CF tests

--- tests/test_rapidpy.py
+++ tests/test_rapidpy.py
@@ -21,6 +21,7 @@ from RAPIDpy.helper_functions import (compare_qout_files,
                                       remove_files,
                                       write_flows_to_csv)
 
+from RAPIDpy.goodness_of_fit import find_goodness_of_fit
 
 #GLOBAL VARIABLES
 MAIN_TESTS_FOLDER = os.path.dirname(os.path.abspath(__file__))
@@ -138,7 +139,7 @@ def test_update_rapid_numbers_input_file():
     rapid_manager.update_reach_number_data()
                           
     rapid_manager.update_parameters(rapid_connect_file='rapid_connect.csv',
-                                    Vlat_file='m3_riv_bas_erai_t511_3hr_19800101to19800101.nc',
+                                    Vlat_file='m3_nasa_lis_3hr_20020830.nc',
                                     riv_bas_id_file='riv_bas_id.csv',
                                     k_file='k.csv',
                                     x_file='x.csv',
@@ -163,8 +164,8 @@ def test_qout_same():
     Test function to compare RAPID simulation Qout
     """
     print "TEST 5: TEST COMPARE RAPID QOUT"
-    input_qout_file = os.path.join(COMPARE_DATA_PATH, 'Qout_erai_t511_3hr_19800101.nc')
-    input_qout_file_cf = os.path.join(COMPARE_DATA_PATH, 'Qout_erai_t511_3hr_19800101_CF.nc')
+    input_qout_file = os.path.join(COMPARE_DATA_PATH, 'Qout_nasa_lis_3hr_20020830.nc')
+    input_qout_file_cf = os.path.join(COMPARE_DATA_PATH, 'Qout_nasa_lis_3hr_20020830_CF.nc')
    
     ok_(compare_qout_files(input_qout_file, input_qout_file_cf))
 
@@ -178,7 +179,7 @@ def test_run_rapid_simulation():
                                              "..", "..",
                                              "rapid", "src", "rapid")
                                              
-    generated_qout_file = os.path.join(OUTPUT_DATA_PATH, 'Qout_erai_t511_3hr_19800101.nc')
+    generated_qout_file = os.path.join(OUTPUT_DATA_PATH, 'Qout_nasa_lis_3hr_20020830_generated.nc')
 
 
 
@@ -186,12 +187,12 @@ def test_run_rapid_simulation():
                           num_processors=1,
                           rapid_connect_file=os.path.join(INPUT_DATA_PATH, 'rapid_connect.csv'),
                           riv_bas_id_file=os.path.join(INPUT_DATA_PATH, 'riv_bas_id.csv'),
-                          Vlat_file=os.path.join(INPUT_DATA_PATH, 'm3_riv_bas_erai_t511_3hr_19800101.nc'),
+                          Vlat_file=os.path.join(INPUT_DATA_PATH, 'm3_nasa_lis_3hr_20020830.nc'),
                           k_file=os.path.join(INPUT_DATA_PATH, 'k.csv'),
                           x_file=os.path.join(INPUT_DATA_PATH, 'x.csv'),
                           ZS_dtM=10800,
                           ZS_dtR=900,
-                          ZS_TauM=86400,
+                          ZS_TauM=2*86400,
                           ZS_TauR=10800,
                           Qout_file=generated_qout_file
                          )
@@ -199,7 +200,7 @@ def test_run_rapid_simulation():
     rapid_manager.run()
 
     generated_qout_file_solution = os.path.join(COMPARE_DATA_PATH,
-                                                'Qout_erai_t511_3hr_19800101.nc')
+                                                'Qout_nasa_lis_3hr_20020830.nc')
 
     #check Qout    
     ok_(compare_qout_files(generated_qout_file, generated_qout_file_solution))
@@ -215,14 +216,14 @@ def test_run_rapid_simulation():
 
     remove_files(generated_qout_file)
     
-def test_convert_file_to_be_cf_compliant():
+def test_convert_file_to_be_cf_compliant_new_format_comid_lat_lon_z():
     """
-    Test Convert RAPID Output to be CF Compliant
+    Test Convert RAPID Output to be CF Compliant for new format with COMID_LAT_LON_Z
     """
     print "TEST 7: TEST CONVERT RAPID OUTPUT TO CF COMPLIANT (COMID_LAT_LON_Z)"
 
-    input_qout_file = os.path.join(COMPARE_DATA_PATH, 'Qout_erai_t511_3hr_19800101.nc')
-    temp_qout_file = os.path.join(OUTPUT_DATA_PATH, 'Qout_erai_t511_3hr_19800101_test_cf.nc')
+    input_qout_file = os.path.join(COMPARE_DATA_PATH, 'Qout_nasa_lis_3hr_20020830.nc')
+    temp_qout_file = os.path.join(OUTPUT_DATA_PATH, 'Qout_nasa_lis_3hr_20020830_test_cf_lat_lon_z.nc')
     copy(input_qout_file, temp_qout_file)
     
     rapid_manager = RAPID(rapid_executable_location="",
@@ -230,12 +231,88 @@ def test_convert_file_to_be_cf_compliant():
                           rapid_connect_file=os.path.join(INPUT_DATA_PATH, 'rapid_connect.csv'),
                           ZS_TauR=3*3600)
 
-    rapid_manager.make_output_CF_compliant(simulation_start_datetime=datetime(1980, 1, 1),
+    rapid_manager.make_output_CF_compliant(simulation_start_datetime=datetime(2002, 8, 30),
                                            comid_lat_lon_z_file=os.path.join(INPUT_DATA_PATH, 'comid_lat_lon_z.csv'),
                                            project_name="ERA Interim (T511 Grid) 3 Hourly Runoff Based Historical flows by US Army ERDC")
 
     cf_qout_file_solution = os.path.join(COMPARE_DATA_PATH,
-                                         'Qout_erai_t511_3hr_19800101_CF.nc')
+                                         'Qout_nasa_lis_3hr_20020830_CF.nc')
+
+    #check Qout    
+    ok_(compare_qout_files(temp_qout_file, cf_qout_file_solution))
+
+    #check other info in netcdf file
+    d1 = Dataset(temp_qout_file)
+    d2 = Dataset(cf_qout_file_solution)
+    ok_(d1.dimensions.keys() == d2.dimensions.keys())
+    ok_(d1.variables.keys() == d2.variables.keys())
+    ok_((d1.variables['time'][:] == d1.variables['time'][:]).all())
+    ok_((d1.variables['rivid'][:] == d1.variables['rivid'][:]).all())
+    ok_((d1.variables['lat'][:] == d1.variables['lat'][:]).all())
+    ok_((d1.variables['lon'][:] == d1.variables['lon'][:]).all())
+    d1.close()
+    d2.close()
+    
+    remove_files(temp_qout_file)
+
+def test_convert_file_to_be_cf_compliant_new_format():
+    """
+    Test Convert RAPID Output to be CF Compliant for new format without COMID_LAT_LON_Z
+    """
+    print "TEST 8: TEST CONVERT RAPID OUTPUT TO CF COMPLIANT (NO COMID_LAT_LON_Z)"
+
+    input_qout_file = os.path.join(COMPARE_DATA_PATH, 'Qout_nasa_lis_3hr_20020830.nc')
+    temp_qout_file = os.path.join(OUTPUT_DATA_PATH, 'Qout_nasa_lis_3hr_20020830_test_cf.nc')
+    copy(input_qout_file, temp_qout_file)
+    
+    rapid_manager = RAPID(rapid_executable_location="",
+                          Qout_file=temp_qout_file,
+                          rapid_connect_file=os.path.join(INPUT_DATA_PATH, 'rapid_connect.csv'),
+                          ZS_TauR=3*3600)
+
+    rapid_manager.make_output_CF_compliant(simulation_start_datetime=datetime(2002, 8, 30),
+                                           comid_lat_lon_z_file="",
+                                           project_name="ERA Interim (T511 Grid) 3 Hourly Runoff Based Historical flows by US Army ERDC")
+
+    cf_qout_file_solution = os.path.join(COMPARE_DATA_PATH,
+                                         'Qout_nasa_lis_3hr_20020830_CF_no_lat_lon_z.nc')
+
+    #check Qout    
+    ok_(compare_qout_files(temp_qout_file, cf_qout_file_solution))
+
+    #check other info in netcdf file
+    d1 = Dataset(temp_qout_file)
+    d2 = Dataset(cf_qout_file_solution)
+    ok_(d1.dimensions.keys() == d2.dimensions.keys())
+    ok_(d1.variables.keys() == d2.variables.keys())
+    ok_((d1.variables['time'][:] == d1.variables['time'][:]).all())
+    ok_((d1.variables['rivid'][:] == d1.variables['rivid'][:]).all())
+    d1.close()
+    d2.close()
+    
+    remove_files(temp_qout_file)
+
+def test_convert_file_to_be_cf_compliant_original_format():
+    """
+    Test Convert RAPID Output to be CF Compliant for original format
+    """
+    print "TEST 9: TEST CONVERT RAPID OUTPUT TO CF COMPLIANT - ORIGINAL (COMID_LAT_LON_Z)"
+
+    input_qout_file = os.path.join(COMPARE_DATA_PATH, 'Qout_nasa_lis_3hr_20020830_original.nc')
+    temp_qout_file = os.path.join(OUTPUT_DATA_PATH, 'Qout_nasa_lis_3hr_20020830_original_test_cf.nc')
+    copy(input_qout_file, temp_qout_file)
+    
+    rapid_manager = RAPID(rapid_executable_location="",
+                          Qout_file=temp_qout_file,
+                          rapid_connect_file=os.path.join(INPUT_DATA_PATH, 'rapid_connect.csv'),
+                          ZS_TauR=3*3600)
+
+    rapid_manager.make_output_CF_compliant(simulation_start_datetime=datetime(2002, 8, 30),
+                                           comid_lat_lon_z_file=os.path.join(INPUT_DATA_PATH, 'comid_lat_lon_z.csv'),
+                                           project_name="ERA Interim (T511 Grid) 3 Hourly Runoff Based Historical flows by US Army ERDC")
+
+    cf_qout_file_solution = os.path.join(COMPARE_DATA_PATH,
+                                         'Qout_nasa_lis_3hr_20020830_CF.nc')
 
     #check Qout    
     ok_(compare_qout_files(temp_qout_file, cf_qout_file_solution))
@@ -258,14 +335,14 @@ def test_generate_qinit_file():
     """
     This tests the qinit file function to create an input qinit file for RAPID
     """
-    print "TEST 8: TEST GENERATE QINIT FILE"
+    print "TEST 10: TEST GENERATE QINIT FILE"
     rapid_manager = RAPID(rapid_executable_location="",
                           rapid_connect_file=os.path.join(INPUT_DATA_PATH, 'rapid_connect.csv')
                          )
 
     #test with original rapid outpur
-    input_qout_file = os.path.join(COMPARE_DATA_PATH, 'Qout_erai_t511_3hr_19800101.nc')
-    original_qout_file = os.path.join(OUTPUT_DATA_PATH, 'Qout_erai_t511_3hr_19800101.nc')
+    input_qout_file = os.path.join(COMPARE_DATA_PATH, 'Qout_nasa_lis_3hr_20020830.nc')
+    original_qout_file = os.path.join(OUTPUT_DATA_PATH, 'Qout_nasa_lis_3hr_20020830.nc')
     copy(input_qout_file, original_qout_file)
 
     qinit_original_rapid_qout = os.path.join(OUTPUT_DATA_PATH, 'qinit_original_rapid_qout.csv')
@@ -276,8 +353,8 @@ def test_generate_qinit_file():
     ok_(fcmp(qinit_original_rapid_qout, qinit_original_rapid_qout_solution))
 
     #test with CF rapid output and alternate time index
-    cf_input_qout_file = os.path.join(COMPARE_DATA_PATH, 'Qout_erai_t511_3hr_19800101_CF.nc')
-    cf_qout_file = os.path.join(OUTPUT_DATA_PATH, 'Qout_erai_t511_3hr_19800101_CF.nc')
+    cf_input_qout_file = os.path.join(COMPARE_DATA_PATH, 'Qout_nasa_lis_3hr_20020830_CF.nc')
+    cf_qout_file = os.path.join(OUTPUT_DATA_PATH, 'Qout_nasa_lis_3hr_20020830_CF.nc')
     copy(cf_input_qout_file, cf_qout_file)
 
     qinit_cf_rapid_qout = os.path.join(OUTPUT_DATA_PATH, 'qinit_cf_rapid_qout.csv')
@@ -298,27 +375,40 @@ def test_extract_timeseries():
     """
     This tests extracting a timeseries from RAPID Qout file
     """
-    print "TEST 9: TEST EXTRACT TIMESERIES FROM QINIT FILE"
+    print "TEST 11: TEST EXTRACT TIMESERIES FROM QINIT FILE"
     
-    #for writing entire time series to file
-    input_qout_file = os.path.join(COMPARE_DATA_PATH, 'Qout_erai_t511_3hr_19800101.nc')
-    original_qout_file = os.path.join(OUTPUT_DATA_PATH, 'Qout_erai_t511_3hr_19800101.nc')
-    copy(input_qout_file, original_qout_file)
-    original_timeseries_file = os.path.join(OUTPUT_DATA_PATH, 'original_timeseries.csv')
+    #for writing entire time series to file from new rapid output
+    input_qout_file = os.path.join(COMPARE_DATA_PATH, 'Qout_nasa_lis_3hr_20020830.nc')
+    new_qout_file = os.path.join(OUTPUT_DATA_PATH, 'Qout_nasa_lis_3hr_20020830.nc')
+    copy(input_qout_file, new_qout_file)
+    new_timeseries_file = os.path.join(OUTPUT_DATA_PATH, 'new_timeseries_file.csv')
     
-    time_valid = write_flows_to_csv(original_qout_file,
-                                    original_timeseries_file,
-                                    reach_id=5785903)
+    time_valid = write_flows_to_csv(new_qout_file,
+                                    new_timeseries_file,
+                                    reach_id=75224)
     if time_valid:
         original_timeseries_file_solution = os.path.join(COMPARE_DATA_PATH, 'original_timeseries-SOLUTION.csv')
     else:
         original_timeseries_file_solution = os.path.join(COMPARE_DATA_PATH, 'original_timeseries-notime-SOLUTION.csv')
         
-    ok_(fcmp(original_timeseries_file, original_timeseries_file_solution))
+    ok_(fcmp(new_timeseries_file, original_timeseries_file_solution))
     
+    #for writing entire time series to file from original rapid output
+    input_qout_file = os.path.join(COMPARE_DATA_PATH, 'Qout_nasa_lis_3hr_20020830_original.nc')
+    original_qout_file = os.path.join(OUTPUT_DATA_PATH, 'Qout_nasa_lis_3hr_20020830_original.nc')
+    copy(input_qout_file, original_qout_file)
+    original_timeseries_file = os.path.join(OUTPUT_DATA_PATH, 'original_timeseries.csv')
+    
+    time_valid = write_flows_to_csv(original_qout_file,
+                                    original_timeseries_file,
+                                    reach_id=75224)
+    original_timeseries_file_solution = os.path.join(COMPARE_DATA_PATH, 'original_timeseries-notime-SOLUTION.csv')
+        
+    ok_(fcmp(original_timeseries_file, original_timeseries_file_solution))
+
     #if file is CF compliant, you can write out daily average
-    cf_input_qout_file = os.path.join(COMPARE_DATA_PATH, 'Qout_erai_t511_3hr_19800101_CF.nc')
-    cf_qout_file = os.path.join(OUTPUT_DATA_PATH, 'Qout_erai_t511_3hr_19800101_CF.nc')
+    cf_input_qout_file = os.path.join(COMPARE_DATA_PATH, 'Qout_nasa_lis_3hr_20020830_CF.nc')
+    cf_qout_file = os.path.join(OUTPUT_DATA_PATH, 'Qout_nasa_lis_3hr_20020830_CF.nc')
     copy(cf_input_qout_file, cf_qout_file)
     cf_timeseries_daily_file = os.path.join(OUTPUT_DATA_PATH, 'cf_timeseries_daily.csv')
 
@@ -339,7 +429,9 @@ def test_extract_timeseries():
     cf_timeseries_file_solution = os.path.join(COMPARE_DATA_PATH, 'cf_timeseries-SOLUTION.csv')    
     ok_(fcmp(cf_timeseries_file, cf_timeseries_file_solution))
 
-    remove_files(original_timeseries_file, 
+    remove_files(new_timeseries_file, 
+                 new_qout_file,
+                 original_timeseries_file, 
                  original_qout_file,
                  cf_timeseries_file,
                  cf_qout_file,
@@ -347,8 +439,45 @@ def test_extract_timeseries():
                  )
 
     
+def test_goodness_of_fit():
+    """
+    This tests the goodness of fit functions
+    """
+    print "TEST 13: TEST GOODNESS OF FIT FUNCTIONS"
     
+    reach_id_file = os.path.join(INPUT_DATA_PATH, 'obs_reach_id.csv') 
+    observed_file = os.path.join(INPUT_DATA_PATH, 'obs_flow.csv') 
+    #using CF-compliant file
+    cf_input_qout_file = os.path.join(COMPARE_DATA_PATH, 'Qout_nasa_lis_3hr_20020830_CF.nc')
+    cf_out_analysis_file = os.path.join(OUTPUT_DATA_PATH, 'cf_goodness_of_fit_results-daily.csv') 
+    find_goodness_of_fit(reach_id_file, cf_input_qout_file, observed_file,
+                         cf_out_analysis_file, daily=True)
 
+    cf_goodness_of_fit_file_solution = os.path.join(COMPARE_DATA_PATH, 'cf_goodness_of_fit_analysis-SOLUTION.csv') 
+    ok_(fcmp(cf_out_analysis_file, cf_goodness_of_fit_file_solution))
+
+    #using original RAPID file
+    original_input_qout_file = os.path.join(COMPARE_DATA_PATH, 'Qout_nasa_lis_3hr_20020830_original.nc')
+    original_out_analysis_file = os.path.join(OUTPUT_DATA_PATH, 'original_goodness_of_fit_results-daily.csv') 
+    find_goodness_of_fit(reach_id_file, original_input_qout_file, observed_file,
+                         original_out_analysis_file, steps_per_group=8)
+
+    raw_goodness_of_fit_file_solution = os.path.join(COMPARE_DATA_PATH, 'raw_goodness_of_fit_analysis-SOLUTION.csv') 
+    ok_(fcmp(original_out_analysis_file, raw_goodness_of_fit_file_solution))
+
+    #using new RAPID file
+    new_input_qout_file = os.path.join(COMPARE_DATA_PATH, 'Qout_nasa_lis_3hr_20020830.nc')
+    new_out_analysis_file = os.path.join(OUTPUT_DATA_PATH, 'goodness_of_fit_results-daily.csv') 
+    find_goodness_of_fit(reach_id_file, new_input_qout_file, observed_file,
+                         new_out_analysis_file, steps_per_group=8)
+
+    new_goodness_of_fit_file_solution = os.path.join(COMPARE_DATA_PATH, 'new_goodness_of_fit_analysis-SOLUTION.csv') 
+    ok_(fcmp(new_out_analysis_file, new_goodness_of_fit_file_solution))
+
+    remove_files(cf_out_analysis_file,
+                 original_out_analysis_file,
+                 new_out_analysis_file)
+    
 if __name__ == '__main__':
     import nose
     nose.main()

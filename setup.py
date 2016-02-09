cb9ff4ee4a47263247b1c405abea695b8c4e68cb
snowman2 <alansnow21@gmail.com>
2016-02-09 11:02:22 -0600

    updated to new RAPIDpy version

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 
 setup(
     name='RAPIDpy',
-    version='2.0.1',
-      description='RAPIDpy is a python interface for RAPID that assists to prepare inputs, runs the RAPID program,'
-                  ' and provides post-processing utilities (https://github.com/erdc-cm/RAPIDpy). More information '
-                  'about installation and the input parameters for RAPID can be found at http://rapid-hub.org. The'
-                  ' source code for RAPID is located at https://github.com/c-h-david/rapid/.',
+    version='2.1.0',
+    description='RAPIDpy is a python interface for RAPID that assists to prepare inputs, runs the RAPID program,'
+                ' and provides post-processing utilities (https://github.com/erdc-cm/RAPIDpy). More information '
+                'about installation and the input parameters for RAPID can be found at http://rapid-hub.org. The'
+                ' source code for RAPID is located at https://github.com/c-h-david/rapid/.',
     keywords='RAPID',
     author='Alan Dee Snow',
     author_email='alan.d.snow@usace.army.mil',
     url='https://github.com/erdc-cm/RAPIDpy',
-    download_url='https://github.com/erdc-cm/RAPIDpy/tarballs/2.0.1',
+    download_url='https://github.com/erdc-cm/RAPIDpy/tarballs/2.1.0',
     license='BSD-3 Clause',
     packages=['RAPIDpy'],
     install_requires=['python-dateutil', 'netCDF4', 'numpy', 'pytz', 'requests'],

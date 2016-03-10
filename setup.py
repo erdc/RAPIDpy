d17f1ca5df1e134eac126dbad0af9572b8a25716
snowman2 <alansnow21@gmail.com>
2016-03-10 08:26:29 -0600

    added zenodo doi badge, updated version to 2.1.2

--- setup.py
+++ setup.py
@@ -2,17 +2,19 @@ from setuptools import setup
 
 setup(
     name='RAPIDpy',
-    version='2.1.1',
+    version='2.1.2',
     description='RAPIDpy is a python interface for RAPID that assists to prepare inputs, runs the RAPID program,'
                 ' and provides post-processing utilities (https://github.com/erdc-cm/RAPIDpy). More information '
                 'about installation and the input parameters for RAPID can be found at http://rapid-hub.org. The'
-                ' source code for RAPID is located at https://github.com/c-h-david/rapid/.',
+                ' source code for RAPID is located at https://github.com/c-h-david/rapid/. \n\n'
+                '.. image:: https://zenodo.org/badge/19918/erdc-cm/RAPIDpy.svg \n'
+                '   :target: https://zenodo.org/badge/latestdoi/19918/erdc-cm/RAPIDpy',
     keywords='RAPID',
     author='Alan Dee Snow',
     author_email='alan.d.snow@usace.army.mil',
     url='https://github.com/erdc-cm/RAPIDpy',
-    download_url='https://github.com/erdc-cm/RAPIDpy/tarballs/2.1.1',
-    license='BSD-3 Clause',
+    download_url='https://github.com/erdc-cm/RAPIDpy/tarballs/2.1.2',
+    license='BSD 3-Clause',
     packages=['RAPIDpy'],
-    install_requires=['python-dateutil', 'netCDF4', 'numpy', 'pytz', 'requests'],
+    install_requires=['numpy', 'netCDF4', 'python-dateutil', 'pytz', 'requests'],
 )

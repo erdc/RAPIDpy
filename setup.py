0f21f7f55ef9ff80ee4d132e85311abb69c235a7
snowman2 <alansnow21@gmail.com>
2016-03-10 08:36:06 -0600

    added long description for zenodo badge

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup
 setup(
     name='RAPIDpy',
     version='2.1.2',
-    description='RAPIDpy is a python interface for RAPID that assists to prepare inputs, runs the RAPID program,'
-                ' and provides post-processing utilities (https://github.com/erdc-cm/RAPIDpy). More information '
-                'about installation and the input parameters for RAPID can be found at http://rapid-hub.org. The'
-                ' source code for RAPID is located at https://github.com/c-h-david/rapid/. \n\n'
-                '.. image:: https://zenodo.org/badge/19918/erdc-cm/RAPIDpy.svg \n'
-                '   :target: https://zenodo.org/badge/latestdoi/19918/erdc-cm/RAPIDpy',
+    description='Python interface for RAPID (rapid-hub.org)',
+    long_description='RAPIDpy is a python interface for RAPID that assists to prepare inputs, runs the RAPID program,'
+                     ' and provides post-processing utilities (https://github.com/erdc-cm/RAPIDpy). More information '
+                     'about installation and the input parameters for RAPID can be found at http://rapid-hub.org. The'
+                     ' source code for RAPID is located at https://github.com/c-h-david/rapid/. \n\n'
+                     '.. image:: https://zenodo.org/badge/19918/erdc-cm/RAPIDpy.svg \n'
+                     '   :target: https://zenodo.org/badge/latestdoi/19918/erdc-cm/RAPIDpy \n\n',
     keywords='RAPID',
     author='Alan Dee Snow',
     author_email='alan.d.snow@usace.army.mil',

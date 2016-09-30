7fdee2ff9a060676ba5836759f0cc0bd161e2228
snowman2 <alan.d.snow@usace.army.mil>
2016-09-30 15:39:49 -0500

    added futures for xrange

--- RAPIDpy/gis/taudem.py
+++ RAPIDpy/gis/taudem.py
@@ -14,6 +14,7 @@ import numpy as np
 import os
 from subprocess import PIPE, Popen
 from sys import getrecursionlimit, setrecursionlimit
+from past.builtins import xrange
 
 try:
     from osgeo import gdal, ogr, osr

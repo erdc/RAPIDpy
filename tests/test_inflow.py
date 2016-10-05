c3f2fd4ecdb3aeacb6e62b40b14a8f2b5db87eaf
snowman2 <alan.d.snow@usace.army.mil>
2016-10-05 11:51:00 -0500

    added test for 24 hr erai3 file

--- tests/test_inflow.py
+++ tests/test_inflow.py
@@ -538,6 +538,62 @@ class TestRAPIDInflow(unittest.TestCase):
         self._compare_m3(generated_m3_file, generated_m3_file_solution)
 
 
+    def test_generate_erai_t511_24_inflow(self):
+        """
+        Checks generating inflow file from ERA Interim t511 24hr LSM
+        """
+        print("TEST 8: TEST GENERATE INFLOW FILE FROM ERA Interim t511 24hr DATA")
+        rapid_input_path, rapid_output_path = self._setup_automated("x-x")
+
+        #run main process
+        self._run_automatic('erai24')
+        
+        #CHECK OUTPUT    
+        #m3_riv
+        m3_file_name = "m3_riv_bas_erai_t511_24hr_19990109to19990110.nc"
+        generated_m3_file = os.path.join(rapid_output_path, m3_file_name)
+        generated_m3_file_solution = os.path.join(self.INFLOW_COMPARE_DATA_PATH, m3_file_name)
+        
+        self._compare_m3(generated_m3_file,generated_m3_file_solution)
+
+        
+    def test_generate_erai_t511_24_inflow2(self):
+        """
+        Checks generating inflow file from ERA Interim t511 24hr LSM manually
+        """
+        print("TEST 8.1: TEST GENERATE INFLOW FILE FROM ERA Interim t511 24hr DATA MANUALLY")
+        
+        rapid_input_path, rapid_output_path = self._setup_manual("x-x")
+
+        lsm_file_list =  sorted(glob(os.path.join(self.LSM_INPUT_DATA_PATH, 'erai24', '*.nc')))                 
+        mp_lock = multiprocessing.Manager().Lock()
+    
+        inf_tool = CreateInflowFileFromERAInterimRunoff()
+    
+        m3_file_name = "m3_riv_bas_erai_t511_24hr_19990109to19990110.nc"
+        generated_m3_file = os.path.join(rapid_output_path, m3_file_name)
+    
+        inf_tool.generateOutputInflowFile(out_nc=generated_m3_file,
+                                          start_datetime_utc=datetime(1999,1,9),
+                                          number_of_timesteps=len(lsm_file_list),
+                                          simulation_time_step_seconds=24*3600,
+                                          in_rapid_connect_file=os.path.join(rapid_input_path, 'rapid_connect.csv'),
+                                          in_rivid_lat_lon_z_file=os.path.join(rapid_input_path, 'comid_lat_lon_z.csv'),
+                                          land_surface_model_description="RAPID Inflow from ERA Interim (T511 Grid) 24 Hourly Runoff",
+                                          modeling_institution="US Army Engineer Research and Development Center"
+                                          )
+                                          
+        inf_tool.execute(nc_file_list=lsm_file_list, 
+                         index_list=list(xrange(len(lsm_file_list))), 
+                         in_weight_table=os.path.join(rapid_input_path, 'weight_era_t511.csv'), 
+                         out_nc=generated_m3_file, 
+                         grid_type='t511', 
+                         mp_lock=mp_lock)
+                          
+        #CHECK OUTPUT
+        generated_m3_file_solution = os.path.join(self.INFLOW_COMPARE_DATA_PATH, m3_file_name)
+        self._compare_m3(generated_m3_file,generated_m3_file_solution)
+
     def _compare_m3(self, generated_m3_file, generated_m3_file_solution):
         #check other info in netcdf file
         d1 = Dataset(generated_m3_file)

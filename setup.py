ef3cfa26b9ed808332b8c02b4ea5b720c13b513d
Alan D. Snow <alan.d.snow@usace.army.mil>
2017-03-08 15:34:17 -0600

    updated description to point to RTD

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@ setup(
     name='RAPIDpy',
     version='2.4.3',
     description='Python interface for RAPID (rapid-hub.org)',
-    long_description='RAPIDpy is a python interface for RAPID that assists to prepare inputs, runs the RAPID program,'
-                     ' and provides post-processing utilities (https://github.com/erdc-cm/RAPIDpy). More information '
-                     'about installation and the input parameters for RAPID can be found at http://rapid-hub.org. The'
-                     ' source code for RAPID is located at https://github.com/c-h-david/rapid/. \n\n'
+    long_description='RAPIDpy is a python interface for RAPID that assists '
+                     'to prepare inputs, runs the RAPID program, and provides '
+                     'post-processing utilities '
+                     '(http://rapidpy.readthedocs.io).
+                     'More information about installation and the input '
+                     'parameters for RAPID can be found at http://rapid-hub.org.'
+                     ' The source code for RAPID is located at '
+                     'https://github.com/c-h-david/rapid/. \n\n'
                      '.. image:: https://zenodo.org/badge/19918/erdc-cm/RAPIDpy.svg \n'
                      '   :target: https://zenodo.org/badge/latestdoi/19918/erdc-cm/RAPIDpy',
     keywords='RAPID',
@@ -17,7 +21,8 @@ setup(
     license='BSD 3-Clause',
     packages=find_packages(),
     package_data={'': ['gis/lsm_grids/*.nc']},
-    install_requires=['future', 'numpy', 'netcdf4', 'pandas', 'python-dateutil', 'pytz', 'requests'],
+    install_requires=['future', 'numpy', 'netcdf4', 'pandas',
+                      'python-dateutil', 'pytz', 'requests'],
     classifiers=[
                 'Intended Audience :: Developers',
                 'Intended Audience :: Science/Research',

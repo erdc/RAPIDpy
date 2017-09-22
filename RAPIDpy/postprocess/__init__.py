69e9b6a1ec3ceb6778f84b929e19b0d067617d9e
snowman2 <alansnow21@gmail.com>
2017-09-22 08:50:37 -0500

    updatet to pass pylint & flake8

--- RAPIDpy/postprocess/__init__.py
+++ RAPIDpy/postprocess/__init__.py
@@ -1,3 +1,11 @@
 # -*- coding: utf-8 -*-
+"""
+    RAPIDpy.postprocess
+
+    Created by Alan D Snow, 2016.
+    Based on RAPID_Toolbox for ArcMap
+    License: BSD 3-Clause
+"""
+# noqa: F401
 from .goodness_of_fit import find_goodness_of_fit, find_goodness_of_fit_csv
 from .merge import ConvertRAPIDOutputToCF
